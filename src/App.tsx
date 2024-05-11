4de10e5358acbaffb66555d46ca7eed140803e42
Christian <christianbutkevicis@gmail.com>
2024-05-11 12:46:03 -0300

    Criado componente services-carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { About } from "./components/about/about";
 import { HomeCarousel } from "./components/carousel/home-carousel";
+import { ServicesCarousel } from "./components/carousel/services-carousel";
 import { Navigation } from "./components/navigation/navigation";
 import "swiper/css";
 import "swiper/css/pagination";
+
+import "swiper/css/effect-creative";
 export function App() {
   return (
     <div className="bg-slate-950 text-slate-200">
       <Navigation />
       <HomeCarousel />
       <About />
+      <ServicesCarousel />
     </div>
   );
 }
---
+++ src/components/carousel/services-carousel.tsx
@@ -0,0 +1,37 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCreative } from "swiper/modules";
+
+export function ServicesCarousel() {
+  return (
+    <div className="max-w-[80vw] m-auto">
+      <Swiper
+        loop
+        grabCursor={true}
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        }}
+        modules={[EffectCreative]}
+      >
+        <SwiperSlide className="bg-red-200 h-96 rounded-lg flex flex-col justify-center items-center text-center ">
+          Slide 1
+        </SwiperSlide>
+        <SwiperSlide className="bg-red-300 h-96 rounded-lg flex flex-col justify-center items-center text-center">
+          Slide 2
+        </SwiperSlide>
+        <SwiperSlide className="bg-red-400 h-96 rounded-lg flex flex-col justify-center items-center text-center">
+          Slide 3
+        </SwiperSlide>
+        <SwiperSlide className="bg-red-500 h-96 rounded-lg flex flex-col justify-center items-center text-center">
+          Slide 4
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
