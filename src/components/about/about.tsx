d1505438f5ad0ef3a16c7ac1bf0c7035e9ab7c7c
Christian <christianbutkevicis@gmail.com>
2024-06-02 22:08:27 -0300

    adicionado responsividade no componente about

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -3,18 +3,28 @@ import logo from "../../assets/pictures/logo.jpeg";
 
 export function About() {
   return (
-    <section className="max-w-[80vw] m-auto py-10 " id="about">
-      <div className="flex flex-col gap-5 justify-center items-center">
-        <img className="rounded-2xl size-40" src={logo} alt="" />
-        <p className="text-center">
-          Bem-vindo à Barbearia Cortes & Estilos, onde tradição e modernidade se
-          encontram para criar um visual impecável. Nossos mestres barbeiros
-          oferecem cortes de cabelo, barbas bem cuidadas e tratamentos de
-          barbearia de luxo, tudo em um ambiente descontraído e acolhedor.
-          Agende sua consulta hoje mesmo e descubra por que somos a escolha
-          preferida dos homens que valorizam qualidade, estilo e tradição.
-        </p>
-        <AgendarButton />
+    <section
+      className="max-w-[80vw] m-auto py-10 xl:max-w-[70vw] 2xl:max-w-[50vw]"
+      id="about"
+    >
+      <div className="flex flex-col gap-5 justify-center items-center sm:flex-row sm:items-start">
+        <img
+          className="rounded-2xl size-40 sm:size-48 lg:size-60"
+          src={logo}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-center gap-4">
+          <p className="text-center md:text-lg">
+            Bem-vindo à Barbearia Cortes & Estilos, onde tradição e modernidade
+            se encontram para criar um visual impecável. Nossos mestres
+            barbeiros oferecem cortes de cabelo, barbas bem cuidadas e
+            tratamentos de barbearia de luxo, tudo em um ambiente descontraído e
+            acolhedor. Agende sua consulta hoje mesmo e descubra por que somos a
+            escolha preferida dos homens que valorizam qualidade, estilo e
+            tradição.
+          </p>
+          <AgendarButton />
+        </div>
       </div>
     </section>
   );
