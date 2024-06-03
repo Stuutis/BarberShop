e97e895bfedd034f1f6b2dd1f0f02c34a66bfb56
Christian <christianbutkevicis@gmail.com>
2024-06-03 13:27:50 -0300

    adicionado responsividade no footer e corrigido altura do card do content carousel

--- src/components/carousel/slide-content.tsx
+++ src/components/carousel/slide-content.tsx
@@ -12,7 +12,7 @@ export function SlideContent({
   titleText,
 }: SlideContentProps) {
   return (
-    <div className="flex flex-col h-[420px] justify-center items-center px-4 py-8 gap-4 sm:justify-between md:h-[540px]">
+    <div className="flex flex-col h-[420px] justify-center items-center px-4 py-8 gap-4 sm:justify-between md:h-[480px]">
       <h1 className="text-xl sm:text-2xl">{titleText}</h1>
       <img className="h-40" src={imageSrc} alt={altText} />
       <p className="sm:text-lg">{contentText}</p>
